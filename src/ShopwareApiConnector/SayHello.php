ec44a234e7334d5dade151b85b4b1ac154eadefd
Willi Helwig <willi.helwig@tivents.de>
2022-01-02 15:18:45 +0100

    init commit

---
+++ src/ShopwareApiConnector/SayHello.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1)
+
+namespace ShopwareApiConnector
+
+class SayHello
+{
+    public static function world(): string
+    {
+        return 'Hello World, Composer!';
+    }
+}
